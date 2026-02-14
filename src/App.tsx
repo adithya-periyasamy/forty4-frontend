4792adcf727a6319d84b7fbfc8e2e9a4101cadf9
Adithya P <adithyaperiyasamy494@gmail.com>
2026-02-14 09:11:37 +0530

    feat: add routing and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import UserDetails from "./pages/UserDetails";
+
 export default function App() {
   return (
-    <>
-      <div>Hello world</div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user/:id" element={<UserDetails />} />
+      </Routes>
+    </Router>
   );
 }
